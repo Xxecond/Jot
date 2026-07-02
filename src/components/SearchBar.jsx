4b57cd2e78370c8aa8967e7b13f72a3383fff636
xxecond <andrewsampadu9@gmail.com>
2026-07-02 10:53:55 +0000

    p-4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@
     <div className="flex justify-end mt-10 pr-10">
       <input
         type="search"
-        placeholder="Search blogs..."
+        placeholder="Search jots..."
         onChange={(e) => setSearchTerm(e.target.value)}
         className="w-2/5 md:w-1/3  placeholder:text-black dark:placeholder:text-white placeholder:tracking-tighter text-black dark:text-white px-4 border border-black dark:border-white rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white focus:border-black dark:focus:border-white transition"
       />
